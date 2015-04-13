f6bc058f3b11bfcbb1a5c99249cd057d100dfebe
browneryan <browneryan87@gmail.com>
2015-04-13 09:48:01 -0700

    minor nav and js changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-$('nav a#toggle').click(function() {
-  $('ul').slideToggle(200, function() {
-    // Animation complete.
-  });
-});
+// $('nav a#toggle').click(function() {
+//   $('ul').slideToggle(200, function() {
+//     // Animation complete.
+//   });
+// });
+//<!--call jPushMenu, required-->
+
+ // (document).ready(function($) {
+ //   $('.toggle-menu').main.js();
+ // });
