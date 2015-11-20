c30a94d7ca047879982008ddce971acdaa23322d
Louise <louise.m.adkins@gmail.com>
2015-11-20 11:34:43 -0800

    cleaned up code, moved script to main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@
  // (document).ready(function($) {
  //   $('.toggle-menu').main.js();
  // });
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+  showLeftPush = document.getElementById( 'showLeftPush' ),
+  body = document.body;
+
+showLeftPush.onclick = function() {
+  classie.toggle( this, 'active' );
+  classie.toggle( body, 'cbp-spmenu-push-toright' );
+  classie.toggle( menuLeft, 'cbp-spmenu-open' );
+  disableOther( 'showLeftPush' );
+};
